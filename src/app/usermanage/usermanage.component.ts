7b67fd9eeab1329e86a4fc48a16f35b0f1a3921b
s0901598 <s0901598@gmail.com>
2025-06-28 15:27:12 +0800

    完成封禁功能、新增玩家查詢篩選列功能

--- src/app/usermanage/usermanage.component.ts
+++ src/app/usermanage/usermanage.component.ts
@@ -10,10 +10,48 @@ import { HttpService } from '../share/service/http.service';
 })
 export class UsermanageComponent {
 
+  searchdata=
+    {
+      username:"",
+      phonenumber:"",
+      isdelete:null as boolean | null
+    }
+  displayStatus = '全部'; // 用於顯示下拉選單的文字
+  loading = false;
   users:any[] = []
   constructor(private http:HttpService){
-    this.http.get('getusers/').subscribe((x:any)=>{
+    this.http.post('getusers/',this.searchdata).subscribe((x:any)=>{
       this.users = x.users;
     })
   }
+
+  //列表是否封禁更改狀態
+  changestus(user:any){
+    user.isdelete = !user.isdelete;
+    const data = {
+      "memberid": user.memberid,
+      "isdelete": user.isdelete
+    }
+    this.http.put('updateuserstatus/',data).subscribe((x:any)=>{
+
+    })
+    console.log(user)
 }
+  getuserfile(){
+    this.http.post('getusers/',this.searchdata).subscribe((x:any)=>{
+      this.users = x.users;
+    })
+  }
+  reset(){
+    this.http.post('getusers/',this.searchdata).subscribe((x:any)=>{
+      this.users = x.users;
+    })
+
+  }
+  // 選擇用戶狀態
+  selectStatus(status: boolean|null) {
+    this.searchdata.isdelete = status;
+    this.displayStatus = status === null ? '全部' : status ? '封禁' : '未封禁';
+  }
+}
+
